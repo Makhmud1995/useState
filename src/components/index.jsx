535a13507e8915220e7ccfa6546f2829daa2c868
Makhmud1995 <yuldoshmakhmud@gamil.com>
2022-08-17 00:34:09 +0500

    hook

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Container,Img,Name,Price } from './style'
 
 const Index = () => {
   return (
-    <div>Index</div>
+    <Container>
+            <Img src="" alt=""/>
+            <Name>Chicken</Name>
+            <Price>Price</Price>
+
+
+
+    </Container>
   )
 }
 
---
+++ src/components/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+
+
+export const Container = styled.div``
+export const Img = styled.img``
+export const Name =styled.div``
+export const Price = styled.div``
